075de4375469059ea118d0b60ecb7eefa56fcd3c
noskojan <37807024+noskojan@users.noreply.github.com>
2020-04-27 22:38:33 +0200

    Update index.js
    Fix starting timer

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 console.log('funguju!');
 
 const inputElm = document.querySelector('.time-input'); //vybírám pole, kam uživatel zadá číslo
-let time = Number(inputElm.value); //převádím vzniklý řetězec na číslo a ukládám do proměnné time
 const alarmElm = document.querySelector('.alarm__seconds') //vybírám pole, kam se propisuje čas
 
 const alarmStart = (event) => {
 
+  let time = Number(inputElm.value); //převádím vzniklý řetězec na číslo a ukládám do proměnné time
   alarmElm.textContent = time;
   
   const countdown = () => {
     time = time - 1;
     if (time === 0) {
+	  console.log('ringing...');
+	  // add ringing class
+	  document.querySelector('.alarm').classList.add('alarm--ring');
       clearInterval(timerId);
     };
   }
